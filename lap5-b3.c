19d4c7140e81929fb8e6da0a6895d84929f35b20
Thanh-o <143236644+Thanh-o@users.noreply.github.com>
2023-09-13 20:57:35 +0700

    Create lap5-b3.c

---
+++ lap5-b3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int i,j,a;
+	printf("Nhap chieu cao: ");
+	scanf("%d",&a);
+	i=0;
+	
+	while(i<a){
+		j=0;
+		while(j<=i){
+		printf("* ");
+		j++;
+}
+       i++;
+      printf("\n"); 
+}
+		
+	
+	return 0;
+}
