1ad13c570c8239c23aa3561fc3355e0a11d65d68
Thanh-o <143236644+Thanh-o@users.noreply.github.com>
2023-09-13 20:42:46 +0700

    Create lap3-b4.c

---
+++ lap3-b4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	float p,r,tienLai;
+	printf("Nhap p: tien von \nNhap n: so nam \nNhap r: ti le lai suat ");
+	tienLai==p*n*r/100;
+	printf("\nHai long chua",tienLai);
+		
+	return 0;
+}
